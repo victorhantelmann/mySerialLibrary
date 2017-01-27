eae643a17400f8330153e5b17e732940ee101696
victorhantelmann <victorhantelmann@yahoo.com>
2017-01-28 01:59:50 +0200

    minor spaces:0000001

--- src/serial_constants.h
+++ src/serial_constants.h
@@ -10,7 +10,7 @@
 
 #include <vector>
 
-static const unsigned int baud_array[] = {0, 50, 75, 110, 134, 150, 200, 300, 600,	1200, 1800, 2400, 4800, 9600, 19200, 38400, 57600, 76800, 115200, 230400, 460800, 500000, 576000, 921600, 1000000, 1152000, 1500000, 2000000, 2500000, 3000000, 3500000, 4000000};
+static const unsigned int baud_array[] = {0, 50, 75, 110, 134, 150, 200, 300, 600, 1200, 1800, 2400, 4800, 9600, 19200, 38400, 57600, 76800, 115200, 230400, 460800, 500000, 576000, 921600, 1000000, 1152000, 1500000, 2000000, 2500000, 3000000, 3500000, 4000000};
 const std::vector<unsigned int> baud_vector (baud_array, baud_array + sizeof(baud_array) / sizeof(baud_array[0]) );
 
 static const unsigned char data_bits_array[] = { 5, 6, 7, 8, 9 };
