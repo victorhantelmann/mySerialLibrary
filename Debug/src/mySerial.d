dcaff2d349bc34f552c8057bfd5d2a99f3e26655
victorhantelmann <victorhantelmann@yahoo.com>
2017-02-02 03:13:42 +0200

    mod merge

---
+++ Debug/src/mySerial.d
@@ -0,0 +1,8 @@
+src/mySerial.o: ../src/mySerial.cpp ../src/serial_routines.h \
+ ../src/serial_errors.h ../src/serial_constants.h
+
+../src/serial_routines.h:
+
+../src/serial_errors.h:
+
+../src/serial_constants.h:
