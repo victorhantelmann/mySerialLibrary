37162c0f4dd1b527493388d6ce723f38e88765ab
victorhantelmann <victorhantelmann@gmail.com>
2017-02-02 02:53:07 +0200

    Update serial_errors.h
    more errors

--- src/serial_errors.h
+++ src/serial_errors.h
@@ -50,6 +50,12 @@ serial_error err_stop_bits_not_set = serial_error(ERR_STOP_BITS_NOT_SET);
 serial_error err_stop_bits_empty_string = serial_error(ERR_STOP_BITS_EMPTY_STRING);
 serial_error err_can_t_read_termios_attribute = serial_error(ERR_CAN_T_READ_TERMIOS_ATTRIBUTE);
 serial_error err_can_t_set_termios_baud = serial_error(ERR_CAN_T_SET_TERMIOS_BAUD);
+serial_error err_can_t_set_termios_NLDLY = serial_error(ERR_CAN_T_SET_TERMIOS_NLDLY);
+serial_error err_can_t_set_termios_CRDLY = serial_error(ERR_CAN_T_SET_TERMIOS_CRDLY);
+serial_error err_can_t_set_termios_TABDLY = serial_error(ERR_CAN_T_SET_TERMIOS_TABDLY);
+serial_error err_can_t_set_termios_VTDLY = serial_error(ERR_CAN_T_SET_TERMIOS_VTDLY);
+serial_error err_can_t_set_termios_FFDLY = serial_error(ERR_CAN_T_SET_TERMIOS_FFDLY);
+serial_error err_can_t_set_termios_CSIZE = serial_error(ERR_CAN_T_SET_TERMIOS_CSIZE);
 serial_error err_can_t_pass_termios_struct = serial_error(ERR_CAN_T_PASS_TERMIOS_STRUCT);
 serial_error ok = serial_error(NO_ERROR);
 
